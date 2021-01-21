172c9bf4d1f9be0756bbe46d3c1ea219aeadabbb
Stefanos Stefanou <stefannstefanou@gmail.com>
2021-01-21 23:10:50 +0000

    gunicorn prod server

--- infobackend/infobackend/settings.py
+++ infobackend/infobackend/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 from pathlib import Path
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -117,3 +117,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
