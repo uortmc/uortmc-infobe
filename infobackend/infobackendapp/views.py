0fa56adbb393047abe950cf59d5e84e943a24c1f
Stefanos Stefanou <stefannstefanou@gmail.com>
2021-01-23 22:57:31 +0000

    testv2

--- infobackend/infobackendapp/views.py
+++ infobackend/infobackendapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpRequest
-
+from django.contrib.auth import authenticate, login
 def index(req:HttpRequest):
     return HttpResponse("Index")
 # Create your views here.
@@ -8,4 +8,9 @@ def index(req:HttpRequest):
 def login(req:HttpRequest):
     username=req.POST['username']
     password=req.POST['password']
-    return HttpResponse("Hello "+username)
+    user=authenticate(req,username=username,password=password)
+    if(user is not None):
+        login(req,user)
+        return HttpResponse("Hello "+username)
+    else:
+        return HttpResponse("Invalid Credentials")
