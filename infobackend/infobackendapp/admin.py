a58d5a888cff5ca994a42df23bade782ef0ac487
Stefanos Stefanou <stefannstefanou@gmail.com>
2021-01-23 21:34:47 +0000

    models subscribed to main admin panel

--- infobackend/infobackendapp/admin.py
+++ infobackend/infobackendapp/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 
-admin.register(models.Doctor)
-admin.register(models.Patient)
-admin.register(models.Scan)
+admin.site.register(models.Doctor)
+admin.site.register(models.Patient)
+admin.site.register(models.Scan)
 
