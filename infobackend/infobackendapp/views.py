ee3be0aa23f82fbc11d7c5a27d54ea36ccd1fa60
Stefanos Stefanou <stefannstefanou@gmail.com>
2021-01-23 22:36:18 +0000

    login initial

--- infobackend/infobackendapp/views.py
+++ infobackend/infobackendapp/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpRequest
 
-def index(req):
-    return HttpResponse("Hello World")
+def index(req:HttpRequest):
+    return HttpResponse("Index")
 # Create your views here.
+
+def login(req:HttpRequest):
+    username=req.POST['username']
+    password=req.POST['password']
+    return HttpResponse("Hello "+username)
