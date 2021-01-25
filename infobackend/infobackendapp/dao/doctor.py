1ccb7c4b71eeb6b3411fa08e42a8905e9b2c2b90
Stefanos Stefanou <stefannstefanou@gmail.com>
2021-01-25 00:47:47 +0000

    finalize

--- infobackend/infobackendapp/dao/doctor.py
+++ infobackend/infobackendapp/dao/doctor.py
@@ -1,7 +1,7 @@
 from ..exceptions.doctordao import UserDoctorAscNotFound
 from ..models import Doctor
 from django.contrib.auth.models import User
-class DoctorDao:
+class DoctorDAO:
 
     def userToDoctor(self,user:User)->Doctor:
         doc=Doctor.objects.filter(username=user.username)
