40446fc279007fd8ef3e4d015df9b44a5ba79f96
Stefanos Stefanou <stefannstefanou@gmail.com>
2021-01-29 02:06:18 +0000

    CORS Middleware enabled

--- infobackend/infobackend/settings.py
+++ infobackend/infobackend/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders'
     'infobackendapp.apps.InformationbackendConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
